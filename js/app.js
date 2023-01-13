ea7ce27723be789cb47afba9e0f4da18736b9919
Martin Aguilar <34758911+martin0123478@users.noreply.github.com>
2023-01-13 13:33:10 -0600

    Mostrando el resultado de la busqueda

--- js/app.js
+++ js/app.js
@@ -29,7 +29,34 @@ function buscarImagenes(termino){
 
 }
  function mostrarImagenes(imagenes){
-    console.log(imagenes)
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild)
+    }
+
+    //Iterar en arreglo
+    imagenes.forEach(imagen => {
+        const {previewURL,likes,views,largeImageURL} = imagen
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 p-3 mb-4">
+            <div class="bg-white">
+             <img class="w-full" src="${previewURL}"/>
+            <div class="p-4">
+                <p class="font-bold">${likes}<span > Me gusta</span></p>
+                <p class="font-bold">${views}<span > Veces vista</span></p>
+
+                <a class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center rounded mt-5 p-1"
+                
+                href="${largeImageURL}" target="_blank" rel="noopener noreferrer">Ver Imagen</a>
+            </div>
+            </div>
+       
+        </div>
+           
+
+
+        `
+        
+    });
 
  }
 
